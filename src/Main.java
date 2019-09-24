eaafa631ece73888aad3c6f9f7d0de015e4de412
tinxian <25557@ma-web.nl>
2019-09-24 11:08:36 +0200

    nbhb

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Running Static Demo");
 
+        StaticDemo anotherDemo = new StaticDemo(10);
         StaticDemo staticDemo = new StaticDemo();
         staticDemo.showMyNumber();
 
